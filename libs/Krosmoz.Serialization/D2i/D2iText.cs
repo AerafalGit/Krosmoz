59cbb1298881ea555e8cd5d8fed7c38eecdf06d0
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-06 22:39:56 +0200

    feat: add D2iText class for representing D2i text entries with generic identifier

---
+++ libs/Krosmoz.Serialization/D2i/D2iText.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Serialization.D2i;
+
+/// <summary>
+/// Represents a D2i text entry.
+/// </summary>
+/// <typeparam name="T">The type of the identifier.</typeparam>
+public sealed class D2iText<T>
+    where T : notnull
+{
+    /// <summary>
+    /// Gets or sets the identifier of the D2i text entry.
+    /// </summary>
+    public T Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the main text of the D2i entry.
+    /// </summary>
+    public string Text { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the non-diacritical text should be used.
+    /// </summary>
+    public bool UseNotDiacriticalText { get; set; }
+
+    /// <summary>
+    /// Gets or sets the non-diacritical version of the text.
+    /// </summary>
+    public string NotDiacriticalText { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="D2iText{T}"/> class with the specified identifier and text.
+    /// </summary>
+    /// <param name="id">The identifier of the D2i text entry.</param>
+    /// <param name="text">The main text of the D2i entry.</param>
+    public D2iText(T id, string text)
+    {
+        Id = id;
+        Text = text;
+        NotDiacriticalText = string.Empty;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="D2iText{T}"/> class with the specified identifier, text, and non-diacritical text.
+    /// </summary>
+    /// <param name="id">The identifier of the D2i text entry.</param>
+    /// <param name="text">The main text of the D2i entry.</param>
+    /// <param name="notDiacriticalText">The non-diacritical version of the text.</param>
+    public D2iText(T id, string text, string notDiacriticalText)
+    {
+        Id = id;
+        Text = text;
+        UseNotDiacriticalText = true;
+        NotDiacriticalText = notDiacriticalText;
+    }
+}

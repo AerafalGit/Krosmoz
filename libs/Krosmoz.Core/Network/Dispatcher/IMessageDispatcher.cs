8fbc558a3c9919c19423d3827114df8c379d23da
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-06 20:24:26 +0200

    feat: add IMessageDispatcher interface for network message handling

---
+++ libs/Krosmoz.Core/Network/Dispatcher/IMessageDispatcher.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Core.Network.Transport;
+
+namespace Krosmoz.Core.Network.Dispatcher;
+
+/// <summary>
+/// Defines a dispatcher for handling and processing network messages.
+/// </summary>
+/// <typeparam name="TMessage">The type of the network message to dispatch.</typeparam>
+public interface IMessageDispatcher<in TMessage>
+    where TMessage : class
+{
+    /// <summary>
+    /// Asynchronously dispatches a network message to the appropriate handler.
+    /// </summary>
+    /// <param name="session">The TCP session associated with the message.</param>
+    /// <param name="message">The network message to dispatch.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task DispatchMessageAsync(TcpSession session, TMessage message);
+}

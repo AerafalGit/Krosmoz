e00636d14dcbc45081d08ec6912b3521f44f2936
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-06 20:22:36 +0200

    feat: add DofusMessageEncoder and DofusMessageDecoder for encoding and decoding Dofus messages

---
+++ libs/Krosmoz.Core/Network/Protocols/Dofus/DofusMessageDecoder.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using System.Buffers;
+using System.Diagnostics.CodeAnalysis;
+using Krosmoz.Core.IO.Binary;
+using Krosmoz.Core.Network.Factory;
+using Krosmoz.Core.Network.Framing;
+
+namespace Krosmoz.Core.Network.Protocols.Dofus;
+
+/// <summary>
+/// Decodes Dofus network messages from a sequence of bytes.
+/// </summary>
+public sealed class DofusMessageDecoder : IMessageDecoder<DofusMessage>
+{
+    private readonly IMessageFactory<DofusMessage> _messageFactory;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DofusMessageDecoder"/> class.
+    /// </summary>
+    /// <param name="messageFactory">The factory used to create Dofus messages.</param>
+    public DofusMessageDecoder(IMessageFactory<DofusMessage> messageFactory)
+    {
+        _messageFactory = messageFactory;
+    }
+
+    /// <summary>
+    /// Attempts to decode a Dofus message from the provided byte sequence.
+    /// </summary>
+    /// <param name="sequence">The sequence of bytes to decode the message from.</param>
+    /// <param name="consumed">The position in the sequence up to which data has been consumed.</param>
+    /// <param name="examined">The position in the sequence up to which data has been examined.</param>
+    /// <param name="message">The decoded Dofus message, or null if decoding fails.</param>
+    /// <returns><c>true</c> if the message was successfully decoded; otherwise, <c>false</c>.</returns>
+    public bool TryDecodeMessage(in ReadOnlySequence<byte> sequence, ref SequencePosition consumed, ref SequencePosition examined, [NotNullWhen(true)] out DofusMessage? message)
+    {
+        message = null;
+
+        using var reader = new BinaryBufferReader(sequence);
+
+        if (!reader.TryReadUShort(out var header))
+            return false;
+
+        if (reader.Remaining < sizeof(int))
+            return false;
+
+        reader.Seek(SeekOrigin.Current, sizeof(int));
+
+        var messageId = (uint)(header >> DofusMessage.BitRightShiftLenPacketId);
+        var messageSize = (byte)(header & DofusMessage.BitMask);
+
+        message = _messageFactory.CreateMessage(messageId);
+
+        if (messageSize is 0)
+        {
+            consumed = sequence.GetPosition(reader.Position);
+            examined = consumed;
+            return true;
+        }
+
+        if (reader.Remaining < messageSize)
+            return false;
+
+        var messageLength = 0;
+
+        for (var i = messageSize - 1; i >= 0; i--)
+            messageLength |= reader.ReadByte() << (i * sizeof(long));
+
+        if (reader.Remaining < messageLength)
+            return false;
+
+        message.Deserialize(reader);
+
+        consumed = sequence.GetPosition(reader.Position);
+        examined = consumed;
+        return true;
+    }
+}
---
+++ libs/Krosmoz.Core/Network/Protocols/Dofus/DofusMessageEncoder.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using System.Buffers;
+using Krosmoz.Core.IO.Binary;
+using Krosmoz.Core.Network.Framing;
+
+namespace Krosmoz.Core.Network.Protocols.Dofus;
+
+/// <summary>
+/// Encodes Dofus network messages into a sequence of bytes.
+/// </summary>
+public sealed class DofusMessageEncoder : IMessageEncoder<DofusMessage>
+{
+    /// <summary>
+    /// Encodes a Dofus message into the provided buffer writer.
+    /// </summary>
+    /// <param name="writer">The buffer writer to write the encoded message to.</param>
+    /// <param name="message">The Dofus message to encode.</param>
+    public void EncodeMessage(IBufferWriter<byte> writer, DofusMessage message)
+    {
+        using var contentWriter = new BinaryBufferWriter();
+
+        message.Serialize(contentWriter);
+
+        var contentBuffer = contentWriter.BufferAsSpan;
+
+        byte messageSize = contentBuffer.Length switch
+        {
+            > ushort.MaxValue => 3,
+            > byte.MaxValue => 2,
+            > 0 => 1,
+            _ => 0
+        };
+
+        var header = (ushort)((message.ProtocolId << DofusMessage.BitRightShiftLenPacketId) | messageSize);
+
+        using var messageWriter = new BinaryBufferWriter();
+
+        messageWriter.WriteUShort(header);
+
+        for (var i = messageSize - 1; i >= 0; i--)
+            messageWriter.WriteByte((byte)((contentBuffer.Length >> (i * sizeof(long))) & byte.MaxValue));
+
+        if (contentBuffer.Length > 0)
+            messageWriter.WriteSpan(contentBuffer);
+
+        writer.Write(messageWriter.BufferAsSpan);
+    }
+}

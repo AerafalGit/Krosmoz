56c9b9455aa1e183bc5aad6ac185e47d78a60316
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-06 22:37:04 +0200

    feat: add Languages enum for supported languages in D2i serialization

---
+++ libs/Krosmoz.Serialization/D2i/Languages.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Serialization.D2i;
+
+/// <summary>
+/// Represents the supported languages for D2i serialization.
+/// </summary>
+public enum Languages
+{
+    /// <summary>
+    /// English language.
+    /// </summary>
+    English,
+
+    /// <summary>
+    /// French language.
+    /// </summary>
+    French,
+
+    /// <summary>
+    /// German language.
+    /// </summary>
+    German,
+
+    /// <summary>
+    /// Spanish language.
+    /// </summary>
+    Spanish,
+
+    /// <summary>
+    /// Italian language.
+    /// </summary>
+    Italian,
+
+    /// <summary>
+    /// Japanese language.
+    /// </summary>
+    Japanese,
+
+    /// <summary>
+    /// Dutch language.
+    /// </summary>
+    Dutch,
+
+    /// <summary>
+    /// Portuguese language.
+    /// </summary>
+    Portuguese,
+
+    /// <summary>
+    /// Russian language.
+    /// </summary>
+    Russian
+}

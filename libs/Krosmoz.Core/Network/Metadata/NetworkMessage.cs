8cbe088a50d9444e0c04fdf4c004c77a3635dfb6
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-06 18:04:06 +0200

    feat: add abstract NetworkMessage class for network message handling

---
+++ libs/Krosmoz.Core/Network/Metadata/NetworkMessage.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Core.IO.Binary;
+
+namespace Krosmoz.Core.Network.Metadata;
+
+/// <summary>
+/// Represents an abstract base class for network messages.
+/// </summary>
+public abstract class NetworkMessage
+{
+    /// <summary>
+    /// Gets the static protocol identifier for the message.
+    /// </summary>
+    public const uint StaticProtocolId = 0;
+
+    /// <summary>
+    /// Gets the protocol identifier for the specific message instance.
+    /// </summary>
+    public abstract uint ProtocolId { get; }
+
+    /// <summary>
+    /// Serializes the message data to the specified binary writer.
+    /// </summary>
+    /// <param name="writer">The binary writer to serialize the message data to.</param>
+    public virtual void Serialize(BinaryBufferStreamWriter writer)
+    {
+    }
+
+    /// <summary>
+    /// Deserializes the message data from the specified binary reader.
+    /// </summary>
+    /// <param name="reader">The binary reader to deserialize the message data from.</param>
+    public virtual void Deserialize(BinaryBufferStreamReader reader)
+    {
+    }
+}

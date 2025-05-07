46b08577e6163b1378b4a7364183ce9f7f00298c
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-07 10:27:07 +0200

    feat: add DatacenterRepository and IDatacenterRepository for Dofus data paths

---
+++ src/Krosmoz.Servers.GameServer/Database/Repositories/Datacenter/DatacenterRepository.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Servers.GameServer.Database.Repositories.Datacenter;
+
+/// <inheritdoc />
+public sealed class DatacenterRepository : IDatacenterRepository
+{
+    /// <inheritdoc />
+    public string DofusPath { get; }
+
+    /// <inheritdoc />
+    public string DofusTilesPath { get; }
+
+    /// <inheritdoc />
+    public string DofusMapsPath { get; }
+
+    /// <inheritdoc />
+    public string DofusCommonPath { get; }
+
+    /// <inheritdoc />
+    public string DofusI18NPath { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DatacenterRepository"/> class.
+    /// Sets up the paths for various Dofus datacenter resources.
+    /// </summary>
+    public DatacenterRepository()
+    {
+        DofusPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Krosmoz");
+        DofusMapsPath = Path.Combine(DofusPath, "content", "maps");
+        DofusTilesPath = Path.Combine(DofusPath, "content", "gfx", "world");
+        DofusCommonPath = Path.Combine(DofusPath, "data", "common");
+        DofusI18NPath = Path.Combine(DofusPath, "data", "i18n");
+    }
+}
---
+++ src/Krosmoz.Servers.GameServer/Database/Repositories/Datacenter/IDatacenterRepository.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Servers.GameServer.Database.Repositories.Datacenter;
+
+/// <summary>
+/// Represents a repository interface for accessing various paths related to the Dofus datacenter.
+/// </summary>
+public interface IDatacenterRepository
+{
+    /// <summary>
+    /// Gets the path to the Dofus game files.
+    /// </summary>
+    string DofusPath { get; }
+
+    /// <summary>
+    /// Gets the path to the Dofus tiles data.
+    /// </summary>
+    string DofusTilesPath { get; }
+
+    /// <summary>
+    /// Gets the path to the Dofus maps data.
+    /// </summary>
+    string DofusMapsPath { get; }
+
+    /// <summary>
+    /// Gets the path to the Dofus common data.
+    /// </summary>
+    string DofusCommonPath { get; }
+
+    /// <summary>
+    /// Gets the path to the Dofus internationalization (I18N) data.
+    /// </summary>
+    string DofusI18NPath { get; }
+}

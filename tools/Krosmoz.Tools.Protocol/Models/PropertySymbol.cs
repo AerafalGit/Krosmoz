4b0d89d3bdd332c3417f766329b97e31bc0bb005
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-07 10:43:01 +0200

    feat: enhance PropertySymbol class with additional properties for better representation

--- tools/Krosmoz.Tools.Protocol/Models/PropertySymbol.cs
+++ tools/Krosmoz.Tools.Protocol/Models/PropertySymbol.cs
@@ -4,7 +4,73 @@
 
 namespace Krosmoz.Tools.Protocol.Models;
 
+/// <summary>
+/// Represents a symbol for a property.
+/// </summary>
 public sealed class PropertySymbol
 {
+    /// <summary>
+    /// Gets or sets the name of the property.
+    /// </summary>
+    public required string Name { get; set; }
 
+    /// <summary>
+    /// Gets or sets the type of the property.
+    /// </summary>
+    public required string Type { get; set; }
+
+    /// <summary>
+    /// Gets or sets the index of the property.
+    /// </summary>
+    public required int Index { get; set; }
+
+    /// <summary>
+    /// Gets or sets the kind of the property, if applicable.
+    /// </summary>
+    public PropertyKind? PropertyKind { get; set; }
+
+    /// <summary>
+    /// Gets or sets the kind of method associated with the property, if applicable.
+    /// </summary>
+    public MethodKind? MethodKind { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the method used to read the property, if applicable.
+    /// </summary>
+    public string? ReadMethod { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the method used to write the property, if applicable.
+    /// </summary>
+    public string? WriteMethod { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the method used to read a vector field, if applicable.
+    /// </summary>
+    public string? VectorFieldRead { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the method used to write a vector field, if applicable.
+    /// </summary>
+    public string? VectorFieldWrite { get; set; }
+
+    /// <summary>
+    /// Gets or sets the object type associated with the property, if applicable.
+    /// </summary>
+    public string? ObjectType { get; set; }
+
+    /// <summary>
+    /// Gets or sets the length of the vector, if the property represents a vector.
+    /// </summary>
+    public int VectorLength { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the property is nullable.
+    /// </summary>
+    public bool Nullable { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the vector uses an ICollection interface.
+    /// </summary>
+    public bool VectorUseICollection { get; set; }
 }

352626d8100b1cac37ace52843f4016690d5ac1f
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-07 21:54:39 +0200

    feat: add regex patterns for matching throw error statements, class declarations, and package declarations

--- tools/Krosmoz.Tools.Protocol/Storages/Expressions/RegexStorage.cs
+++ tools/Krosmoz.Tools.Protocol/Storages/Expressions/RegexStorage.cs
@@ -6,6 +6,29 @@ using System.Text.RegularExpressions;
 
 namespace Krosmoz.Tools.Protocol.Storages.Expressions;
 
+/// <summary>
+/// Provides a collection of precompiled regular expressions for parsing protocol-related constructs.
+/// </summary>
 public static partial class RegexStorage
 {
+    /// <summary>
+    /// Matches throw error statements in the source code.
+    /// </summary>
+    /// <returns>A <see cref="Regex"/> for matching throw error statements.</returns>
+    [GeneratedRegex(@"^\s*throw\s+new\s+Error\s*\(", RegexOptions.Multiline)]
+    public static partial Regex ThrowError();
+
+    /// <summary>
+    /// Matches class declarations, including optional parent and interface information.
+    /// </summary>
+    /// <returns>A <see cref="Regex"/> for matching class declarations.</returns>
+    [GeneratedRegex(@"public\s*[final]*\s+class\s+(?<name>[\w]+)\s*[extends]*\s*(?<parent>[\w]*)\s[implements\s+]*(?<interface>[\w]*)\s*,*\s*(?<interface2>[\w]*)", RegexOptions.Multiline)]
+    public static partial Regex ClassDeclaration();
+
+    /// <summary>
+    /// Matches package declarations in the source code.
+    /// </summary>
+    /// <returns>A <see cref="Regex"/> for matching package declarations.</returns>
+    [GeneratedRegex(@"package\s+(?<name>[\w|\.]+)", RegexOptions.Multiline)]
+    public static partial Regex NamespaceDeclaration();
 }

18559d704e5edacc1c2c54fac98126b11a9af6b6
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-07 22:36:29 +0200

    feat: implement EnumConverter for transforming enumeration symbols and properties

---
+++ tools/Krosmoz.Tools.Protocol/Converters/EnumConverter.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Tools.Protocol.Extensions;
+using Krosmoz.Tools.Protocol.Models;
+
+namespace Krosmoz.Tools.Protocol.Converters;
+
+/// <summary>
+/// Converts enumeration symbols by modifying their metadata and properties.
+/// </summary>
+public sealed class EnumConverter : IConverter<EnumSymbol>
+{
+    /// <summary>
+    /// Converts the provided enumeration symbol by updating its metadata name
+    /// and transforming its properties to adhere to specific naming and type conventions.
+    /// </summary>
+    /// <param name="symbol">The enumeration symbol to convert.</param>
+    public void Convert(EnumSymbol symbol)
+    {
+        symbol.Metadata.Name = symbol.Metadata.Name
+            .Replace("Enum", string.Empty)
+            .Pluralize();
+
+        foreach (var propertySymbol in symbol.Properties)
+        {
+            propertySymbol.Name = propertySymbol.Name.ToPascalCase();
+
+            if (propertySymbol.Type is "Number")
+                propertySymbol.Type = "long";
+        }
+    }
+}

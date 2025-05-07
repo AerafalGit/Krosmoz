5d9a8f763e6efbcd2619331d11b26d971293f47f
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-07 23:44:19 +0200

    feat: add DebuggerDisplay attribute and override ToString method in EnumPropertySymbol for improved debugging

--- tools/Krosmoz.Tools.Protocol/Models/EnumPropertySymbol.cs
+++ tools/Krosmoz.Tools.Protocol/Models/EnumPropertySymbol.cs
@@ -2,11 +2,14 @@
 // Krosmoz licenses this file to you under the MIT license.
 // See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
 
+using System.Diagnostics;
+
 namespace Krosmoz.Tools.Protocol.Models;
 
 /// <summary>
 /// Represents a symbol for an enumeration property.
 /// </summary>
+[DebuggerDisplay("{ToString(),nq}")]
 public sealed class EnumPropertySymbol
 {
     /// <summary>
@@ -23,4 +26,14 @@ public sealed class EnumPropertySymbol
     /// Gets or sets the value of the enumeration property.
     /// </summary>
     public required long Value { get; set; }
+
+    /// <summary>
+    /// Returns a string representation of the enumeration property symbol,
+    /// including its name, type, and value.
+    /// </summary>
+    /// <returns>A string describing the enumeration property symbol.</returns>
+    public override string ToString()
+    {
+        return $"Name: {Name}, Type: {Type}, Value: {Value}";
+    }
 }

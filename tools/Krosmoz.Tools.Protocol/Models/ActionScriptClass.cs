8171bd659d201b44dee334a5b4d75f759bf01b25
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-07 10:34:13 +0200

    feat: add ActionScriptClass to represent ActionScript classes with metadata and properties

---
+++ tools/Krosmoz.Tools.Protocol/Models/ActionScriptClass.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Tools.Protocol.Models;
+
+/// <summary>
+/// Represents an ActionScript class.
+/// </summary>
+public sealed class ActionScriptClass
+{
+    /// <summary>
+    /// Gets or sets the metadata associated with the ActionScript class.
+    /// </summary>
+    public required ActionScriptClassMetadata Metadata { get; set; }
+
+    /// <summary>
+    /// Gets or sets the protocol identifier for the ActionScript class.
+    /// </summary>
+    public required int ProtocolId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the list of namespaces or libraries used by the ActionScript class.
+    /// </summary>
+    public required List<string> Usings { get; set; }
+
+    /// <summary>
+    /// Gets or sets the dictionary of properties defined in the ActionScript class.
+    /// The key represents the property name, and the value represents the property details.
+    /// </summary>
+    public required Dictionary<string, ActionScriptProperty> Properties { get; set; }
+}

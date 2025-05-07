7658e5eebcda58bc738ec0a3681bebeb69bbb382
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-07 10:39:49 +0200

    feat: add EnumSymbol and EnumPropertySymbol classes to represent enumeration symbols and their properties

---
+++ tools/Krosmoz.Tools.Protocol/Models/EnumPropertySymbol.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Tools.Protocol.Models;
+
+/// <summary>
+/// Represents a symbol for an enumeration property.
+/// </summary>
+public sealed class EnumPropertySymbol
+{
+    /// <summary>
+    /// Gets or sets the name of the enumeration property.
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the type of the enumeration property.
+    /// </summary>
+    public required string Type { get; set; }
+
+    /// <summary>
+    /// Gets or sets the value of the enumeration property.
+    /// </summary>
+    public required long Value { get; set; }
+}
---
+++ tools/Krosmoz.Tools.Protocol/Models/EnumSymbol.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Tools.Protocol.Models;
+
+/// <summary>
+/// Represents a symbol for an enumeration.
+/// </summary>
+public sealed class EnumSymbol
+{
+    /// <summary>
+    /// Gets or sets the metadata associated with the enumeration symbol.
+    /// </summary>
+    public required SymbolMetadata Metadata { get; set; }
+
+    /// <summary>
+    /// Gets or sets the list of properties associated with the enumeration symbol.
+    /// Each property represents a specific value in the enumeration.
+    /// </summary>
+    public required List<EnumPropertySymbol> Properties { get; set; }
+}

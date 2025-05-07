672f298feea7cb0f6a8cb2a169adc39007483306
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-07 10:32:18 +0200

    feat: add ActionScriptClassMetadata class to represent metadata for ActionScript classes

---
+++ tools/Krosmoz.Tools.Protocol/Models/ActionScriptClassMetadata.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Tools.Protocol.Models;
+
+/// <summary>
+/// Represents metadata for an ActionScript class.
+/// </summary>
+public sealed class ActionScriptClassMetadata
+{
+    /// <summary>
+    /// Gets or sets the namespace of the ActionScript class.
+    /// </summary>
+    public required string Namespace { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the ActionScript class.
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the parent class of the ActionScript class.
+    /// </summary>
+    public required string ParentName { get; set; }
+
+    /// <summary>
+    /// Gets or sets the source file of the ActionScript class.
+    /// </summary>
+    public required string Source { get; set; }
+
+    /// <summary>
+    /// Gets or sets the type of the ActionScript class.
+    /// </summary>
+    public required ActionScriptClassTypes Type { get; set; }
+}

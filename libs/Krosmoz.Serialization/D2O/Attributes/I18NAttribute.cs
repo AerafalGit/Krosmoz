0f0970e0412e1ce3a5ab9b7b48cfdc0f9b814f3d
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-09 00:06:47 +0200

    feat: add I18NAttribute to specify internationalized property values

---
+++ libs/Krosmoz.Serialization/D2O/Attributes/I18NAttribute.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Serialization.D2O.Attributes;
+
+/// <summary>
+/// Specifies that a property is associated with an internationalized (I18N) value.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public sealed class I18NAttribute : Attribute;

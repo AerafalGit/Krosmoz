6e74914842df4cb2bcab01de363fb1ab006bea06
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-09 00:05:23 +0200

    feat: add IDatacenterObject interface with static abstract ModuleName property

---
+++ libs/Krosmoz.Serialization/D2O/Abstractions/IDatacenterObject.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Serialization.D2O.Abstractions;
+
+/// <summary>
+/// Represents a datacenter object with a static abstract property for the module name.
+/// </summary>
+public interface IDatacenterObject
+{
+    /// <summary>
+    /// Gets the name of the module associated with the datacenter object.
+    /// </summary>
+    static abstract string ModuleName { get; }
+}

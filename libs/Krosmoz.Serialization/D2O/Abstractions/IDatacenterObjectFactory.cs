a5880330146e9053fd56191320d8ed06ae14135e
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-09 10:55:31 +0200

    feat: implement IDatacenterObjectFactory and DatacenterObjectFactoryRenderer for datacenter object creation

---
+++ libs/Krosmoz.Serialization/D2O/Abstractions/IDatacenterObjectFactory.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Serialization.D2O.Abstractions;
+
+/// <summary>
+/// Defines a factory interface for creating datacenter objects.
+/// </summary>
+public interface IDatacenterObjectFactory
+{
+    /// <summary>
+    /// Creates a datacenter object based on the specified name.
+    /// </summary>
+    /// <param name="name">The name of the datacenter object to create.</param>
+    /// <returns>An instance of <see cref="IDatacenterObject"/> representing the created object.</returns>
+    IDatacenterObject Create(string name);
+}

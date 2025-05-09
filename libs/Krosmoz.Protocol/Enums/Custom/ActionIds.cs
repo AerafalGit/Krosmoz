5d44547d25d0b258e48f51ce189b16b084b2db45
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-09 14:49:19 +0200

    refactor: rename namespaces to Krosmoz.Protocol.Enums.Custom

--- libs/Krosmoz.Protocol/Enums/Custom/ActionIds.cs
+++ libs/Krosmoz.Protocol/Enums/Custom/ActionIds.cs
@@ -2,7 +2,7 @@
 // Krosmoz licenses this file to you under the MIT license.
 // See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
 
-namespace Krosmoz.Protocol.Enums;
+namespace Krosmoz.Protocol.Enums.Custom;
 
 public enum ActionIds : int
 {

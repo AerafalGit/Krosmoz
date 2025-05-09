e9ade3fe0667a595db6169a710d5b2c43a933221
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-09 08:20:45 +0200

    feat: extend IDatacenterObject interface with deserialization method

--- libs/Krosmoz.Serialization/D2O/Abstractions/IDatacenterObject.cs
+++ libs/Krosmoz.Serialization/D2O/Abstractions/IDatacenterObject.cs
@@ -2,15 +2,30 @@
 // Krosmoz licenses this file to you under the MIT license.
 // See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
 
+using Krosmoz.Core.IO.Binary;
+
 namespace Krosmoz.Serialization.D2O.Abstractions;
 
 /// <summary>
-/// Represents a datacenter object with a static abstract property for the module name.
+/// Represents a datacenter object with a static abstract property for the module name
+/// and a method for deserializing the object from a D2O class and binary reader.
 /// </summary>
-public interface IDatacenterObject
+/// <typeparam name="T">
+/// The type of the datacenter object, which must implement <see cref="IDatacenterObject{T}"/>.
+/// </typeparam>
+public interface IDatacenterObject<out T>
+    where T : class, IDatacenterObject<T>
 {
     /// <summary>
     /// Gets the name of the module associated with the datacenter object.
     /// </summary>
     static abstract string ModuleName { get; }
+
+    /// <summary>
+    /// Deserializes the datacenter object from the specified D2O class and binary reader.
+    /// </summary>
+    /// <param name="d2OClass">The D2O class containing metadata for deserialization.</param>
+    /// <param name="reader">The binary reader used to read the object's data.</param>
+    /// <returns>An instance of the datacenter object.</returns>
+    static abstract T Deserialize(D2OClass d2OClass, BigEndianReader reader);
 }

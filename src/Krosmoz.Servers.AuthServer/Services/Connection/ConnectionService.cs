f11c8138145b2d8466f371332da1c859aae9960b
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-11 00:01:37 +0200

    feat: implement connection service

---
+++ src/Krosmoz.Servers.AuthServer/Services/Connection/ConnectionService.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Protocol.Enums;
+using Krosmoz.Protocol.Messages.Connection;
+using Krosmoz.Servers.AuthServer.Network.Transport;
+
+namespace Krosmoz.Servers.AuthServer.Services.Connection;
+
+/// <summary>
+/// Provides services for managing connection-related operations in the authentication server.
+/// </summary>
+public sealed class ConnectionService : IConnectionService
+{
+    /// <summary>
+    /// Sends an identification failure message asynchronously to the specified session.
+    /// </summary>
+    /// <param name="session">The session to which the failure message will be sent.</param>
+    /// <param name="reason">The reason for the identification failure.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    public async Task SendIdentificationFailedMessageAsync(AuthSession session, IdentificationFailureReasa reason)
+    {
+        await session.SendAsync(new IdentificationFailedMessage { Reason = (sbyte)reason });
+    }
+}
---
+++ src/Krosmoz.Servers.AuthServer/Services/Connection/IConnectionService.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Protocol.Enums;
+using Krosmoz.Servers.AuthServer.Network.Transport;
+
+namespace Krosmoz.Servers.AuthServer.Services.Connection;
+
+/// <summary>
+/// Defines the contract for a connection service that handles connection-related operations.
+/// </summary>
+public interface IConnectionService
+{
+    /// <summary>
+    /// Sends an identification failure message asynchronously to the specified session.
+    /// </summary>
+    /// <param name="session">The session to which the failure message will be sent.</param>
+    /// <param name="reason">The reason for the identification failure.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task SendIdentificationFailedMessageAsync(AuthSession session, IdentificationFailureReasa reason);
+}

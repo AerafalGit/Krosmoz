86ef581f66e835bae241e6e1105ab84c72b4da13
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-10 22:32:09 +0200

    feat: add ServerRecord entity and configuration for database schema

---
+++ src/Krosmoz.Servers.AuthServer/Database/Models/Servers/ServerRecord.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using System.Net;
+using Krosmoz.Protocol.Enums;
+using Krosmoz.Protocol.Enums.Custom;
+
+namespace Krosmoz.Servers.AuthServer.Database.Models.Servers;
+
+public sealed class ServerRecord
+{
+    public required int Id { get; init; }
+
+    public required string Name { get; init; }
+
+    public required ServerGameTypeIds Type { get; init; }
+
+    public required ServerStatus Status { get; set; }
+
+    public IPAddress? IpAddress { get; set; }
+
+    public int? Port { get; set; }
+
+    public DateTime? OpenedAt { get; set; }
+}

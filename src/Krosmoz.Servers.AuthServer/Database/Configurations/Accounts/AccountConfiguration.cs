2db5d0d9c11431798ea52ee8a1866bec568ff983
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-10 23:23:38 +0200

    feat: add entity configurations for Account and ServerCharacter records

---
+++ src/Krosmoz.Servers.AuthServer/Database/Configurations/Accounts/AccountConfiguration.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Core.Extensions;
+using Krosmoz.Servers.AuthServer.Database.Models.Accounts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Krosmoz.Servers.AuthServer.Database.Configurations.Accounts;
+
+/// <summary>
+/// Configures the entity type for <see cref="AccountRecord"/> in the database.
+/// </summary>
+public sealed class AccountConfiguration : IEntityTypeConfiguration<AccountRecord>
+{
+    /// <summary>
+    /// Configures the properties and relationships of the <see cref="AccountRecord"/> entity.
+    /// </summary>
+    /// <param name="builder">The builder used to configure the entity type.</param>
+    public void Configure(EntityTypeBuilder<AccountRecord> builder)
+    {
+        builder.HasKey(static x => x.Id);
+
+        builder
+            .Property(static x => x.Id)
+            .ValueGeneratedOnAdd();
+
+        builder
+            .Property(static x => x.Username)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.Password)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.Hierarchy)
+            .HasEnumToStringConversion()
+            .IsRequired();
+
+        builder
+            .Property(static x => x.SecretQuestion)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.SecretAnswer)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.CreatedAt)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.UpdatedAt)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.Characters)
+            .IsRequired();
+
+        builder
+            .HasMany(static x => x.Characters)
+            .WithOne()
+            .HasForeignKey(static x => x.AccountId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder
+            .HasIndex(static x => new { x.Username, x.Nickname })
+            .IsUnique();
+
+        builder.ToTable("accounts");
+    }
+}
---
+++ src/Krosmoz.Servers.AuthServer/Database/Models/Accounts/AccountRecord.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using System.Net;
+using Krosmoz.Protocol.Enums;
+using Krosmoz.Servers.AuthServer.Database.Models.Servers;
+
+namespace Krosmoz.Servers.AuthServer.Database.Models.Accounts;
+
+public sealed class AccountRecord
+{
+    public required int Id { get; init; }
+
+    public required string Username { get; set; }
+
+    public required string Password { get; set; }
+
+    public required GameHierarchies Hierarchy { get; set; }
+
+    public required string SecretQuestion { get; set; }
+
+    public required string SecretAnswer { get; set; }
+
+    public required DateTime CreatedAt { get; init; }
+
+    public required DateTime UpdatedAt { get; set; }
+
+    public required List<ServerCharacterRecord> Characters { get; set; }
+
+    public string? Nickname { get; set; }
+
+    public IPAddress? IpAddress { get; set; }
+
+    public string? Token { get; set; }
+}

55c20f47b491a9eceb4fbe5c04f7951ead868344
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-10 22:35:15 +0200

    feat: add migration and model snapshot for ServerRecord entity

---
+++ src/Krosmoz.Servers.AuthServer/Database/Migrations/20250510203454_servers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Krosmoz.Servers.AuthServer.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class servers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "servers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    IpAddress = table.Column<IPAddress>(type: "inet", nullable: true),
+                    Port = table.Column<int>(type: "integer", nullable: true),
+                    OpenedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_servers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "servers");
+        }
+    }
+}

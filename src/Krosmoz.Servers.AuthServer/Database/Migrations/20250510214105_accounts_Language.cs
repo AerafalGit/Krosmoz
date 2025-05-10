6f28f493c1016005f049843268724aea0aa53c40
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-10 23:42:06 +0200

    feat: add Language property to AuthDbContextModelSnapshot

---
+++ src/Krosmoz.Servers.AuthServer/Database/Migrations/20250510214105_accounts_Language.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Krosmoz.Servers.AuthServer.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class accounts_Language : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "accounts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "accounts");
+        }
+    }
+}

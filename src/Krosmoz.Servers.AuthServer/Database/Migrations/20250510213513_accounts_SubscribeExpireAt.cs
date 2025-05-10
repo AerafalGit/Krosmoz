3ee49336270a51d96eb0bac418d53b81aebd75c4
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-10 23:35:24 +0200

    feat: add SubscriptionExpireAt property to AccountRecord in model snapshot

---
+++ src/Krosmoz.Servers.AuthServer/Database/Migrations/20250510213513_accounts_SubscribeExpireAt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Krosmoz.Servers.AuthServer.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class accounts_SubscribeExpireAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SubscriptionExpireAt",
+                table: "accounts",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubscriptionExpireAt",
+                table: "accounts");
+        }
+    }
+}

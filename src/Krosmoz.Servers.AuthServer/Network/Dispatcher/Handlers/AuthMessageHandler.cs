83d0fdf6dafc9ffc8ebc656dea98e860b949eec1
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-10 20:36:38 +0200

    feat: implement authentication server with message handling and session management

---
+++ src/Krosmoz.Servers.AuthServer/Network/Dispatcher/Handlers/AuthMessageHandler.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Core.Network.Dispatcher.Handlers;
+using Krosmoz.Core.Network.Protocol.Dofus;
+using Krosmoz.Servers.AuthServer.Network.Transport;
+
+namespace Krosmoz.Servers.AuthServer.Network.Dispatcher.Handlers;
+
+/// <summary>
+/// Represents an abstract base class for handling authentication-related messages.
+/// </summary>
+/// <typeparam name="TMessage">The type of the Dofus message being handled.</typeparam>
+public abstract class AuthMessageHandler<TMessage> : MessageHandler<AuthSession, TMessage, DofusMessage>
+    where TMessage : DofusMessage;

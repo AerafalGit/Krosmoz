d7855bf651045e34b751434a389f01ce2185b52a
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-10 17:12:29 +0200

    feat: add ToMd5 method for string hashing and mark existing methods as pure

--- libs/Krosmoz.Core/Extensions/StringExtensions.cs
+++ libs/Krosmoz.Core/Extensions/StringExtensions.cs
@@ -2,7 +2,9 @@
 // Krosmoz licenses this file to you under the MIT license.
 // See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
 
+using System.Diagnostics.Contracts;
 using System.Globalization;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Krosmoz.Core.Extensions;
@@ -17,6 +19,7 @@ public static class StringExtensions
     /// </summary>
     /// <param name="text">The string to capitalize.</param>
     /// <returns>The input string with its first character converted to uppercase.</returns>
+    [Pure]
     public static string Capitalize(this string text)
     {
         return string.IsNullOrEmpty(text)
@@ -29,6 +32,7 @@ public static class StringExtensions
     /// </summary>
     /// <param name="text">The string to check for accents.</param>
     /// <returns><c>true</c> if the string contains accented characters; otherwise, <c>false</c>.</returns>
+    [Pure]
     public static bool HasAccents(this string text)
     {
         return text
@@ -41,11 +45,23 @@ public static class StringExtensions
     /// </summary>
     /// <param name="text">The string from which to remove accents.</param>
     /// <returns>A new string with all accented characters removed.</returns>
+    [Pure]
     public static string RemoveAccents(this string text)
     {
         return string.Concat(text
                 .Normalize(NormalizationForm.FormD)
                 .Where(static c => CharUnicodeInfo.GetUnicodeCategory(c) is not UnicodeCategory.NonSpacingMark))
             .Normalize(NormalizationForm.FormC);
+    }
+
+    /// <summary>
+    /// Converts the specified string to its MD5 hash representation in lowercase hexadecimal format.
+    /// </summary>
+    /// <param name="text">The input string to hash.</param>
+    /// <returns>A string containing the MD5 hash of the input, represented in lowercase hexadecimal format.</returns>
+    [Pure]
+    public static string ToMd5(this string text)
+    {
+        return Convert.ToHexStringLower(MD5.HashData(Encoding.UTF8.GetBytes(text)));
     }
 }

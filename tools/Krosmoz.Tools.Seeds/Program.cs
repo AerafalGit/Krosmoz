8ff162df8cf29faddce47dcc573699711f391ec3
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-05 17:47:03 +0200

    feat: initialize seeding tool with database contexts and services

--- tools/Krosmoz.Tools.Seeds/Program.cs
+++ tools/Krosmoz.Tools.Seeds/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Krosmoz.Core.Extensions;
+using Krosmoz.Servers.AuthServer.Database;
+using Krosmoz.Servers.GameServer.Database;
+using Krosmoz.Servers.GameServer.Services.Datacenter;
+using Krosmoz.Tools.Seeds;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-Console.WriteLine("Hello, World!");
+var builder = Host.CreateApplicationBuilder(args);
+
+builder
+    .AddServiceDefaults()
+    .UseNpgsqlDbContext<AuthDbContext>("krosmoz-auth-db")
+    .UseNpgsqlDbContext<GameDbContext>("krosmoz-game-db");
+
+builder.Services
+    .AddSingleton<IDatacenterService, DatacenterService>()
+    .AddHostedService<SeedWorker>();
+
+var app = builder.Build();
+
+app.Run();

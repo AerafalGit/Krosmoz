8b781017d311f1ca6bb46bbc5904d961b9a9d4b6
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-05 21:59:37 +0200

    feat: initialize AuthServer with services, protocols, and database config

--- src/Krosmoz.Servers.AuthServer/Program.cs
+++ src/Krosmoz.Servers.AuthServer/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Krosmoz.Core.Extensions;
+using Krosmoz.Core.Network.Hosting;
+using Krosmoz.Core.Network.Protocol.Dofus;
+using Krosmoz.Core.Scheduling;
+using Krosmoz.Servers.AuthServer.Database;
+using Krosmoz.Servers.AuthServer.Network.Transport;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-Console.WriteLine("Hello, World!");
+var builder = Host.CreateApplicationBuilder(args);
+
+builder
+    .AddServiceDefaults()
+    .UseNpgsqlDbContext<AuthDbContext>("krosmoz-auth-db")
+    .UseNats("krosmoz-nats")
+    .UseCompositeServer(static server => server.ListenFromEnvironment(static connection => connection.UseConnectionHandler<DofusConnectionHandler>()));
+
+builder.Services
+    .AddDofusProtocol()
+    .AddTransient<IScheduler, Scheduler>();
+
+builder
+    .Build()
+    .Run();

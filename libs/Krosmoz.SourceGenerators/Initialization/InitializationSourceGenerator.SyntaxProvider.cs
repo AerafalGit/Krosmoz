ec056b49985a4d14c54e77f297c87e62e5c6ae45
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-05 22:17:41 +0200

    feat: add Initialization source generator

---
+++ libs/Krosmoz.SourceGenerators/Initialization/InitializationSourceGenerator.SyntaxProvider.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.SourceGenerators.Initialization.Generated;
+using Krosmoz.SourceGenerators.Initialization.Models;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Krosmoz.SourceGenerators.Initialization;
+
+public sealed partial class InitializationSourceGenerator
+{
+    /// <summary>
+    /// Determines whether the specified syntax node matches the criteria for initialization services.
+    /// </summary>
+    /// <param name="syntaxNode">The syntax node to evaluate.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>
+    /// <c>true</c> if the syntax node is a class declaration that implements
+    /// <b>IInitializableService</b> or <b>IAsyncInitializableService</b>; otherwise, <c>false</c>.
+    /// </returns>
+    private static bool Predicate(SyntaxNode syntaxNode, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        return syntaxNode is ClassDeclarationSyntax { BaseList: not null } declarationSyntax &&
+               declarationSyntax.BaseList.Types.Any(static t => t.Type.ToString() is nameof(IInitializableService) or nameof(IAsyncInitializableService));
+    }
+
+    /// <summary>
+    /// Transforms a syntax node into an <see cref="InitializableService"/> object.
+    /// </summary>
+    /// <param name="context">The syntax context containing the node and semantic model.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>An <see cref="InitializableService"/> object representing the transformed syntax node.</returns>
+    /// <exception cref="Exception">Thrown if the symbol cannot be retrieved from the syntax node.</exception>
+    private static InitializableService Transform(GeneratorSyntaxContext context, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var symbol = (INamedTypeSymbol)context.SemanticModel.GetDeclaredSymbol(context.Node)!;
+
+        var symbolName = symbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+
+        var @interface = symbol.Interfaces.First(x => x.Name is nameof(IInitializableService) or nameof(IAsyncInitializableService));
+
+        var isAsync = @interface.Name.Contains("Async");
+
+        return new InitializableService(symbolName, isAsync);
+    }
+}
---
+++ libs/Krosmoz.SourceGenerators/Initialization/InitializationSourceGenerator.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using System.Text;
+using Krosmoz.SourceGenerators.Initialization.Generated;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Krosmoz.SourceGenerators.Initialization;
+
+/// <summary>
+/// Represents a source generator for initialization services.
+/// </summary>
+[Generator(LanguageNames.CSharp)]
+public sealed partial class InitializationSourceGenerator : IIncrementalGenerator
+{
+    /// <summary>
+    /// Initializes the source generator by registering the necessary steps in the generation pipeline.
+    /// </summary>
+    /// <param name="context">The context for incremental generator initialization.</param>
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(static ctx =>
+        {
+            ctx.AddSource(IInitializableService.GlobalName, SourceText.From(IInitializableService.Source, Encoding.UTF8));
+            ctx.AddSource(IAsyncInitializableService.GlobalName, SourceText.From(IAsyncInitializableService.Source, Encoding.UTF8));
+        });
+
+        var syntaxProvider = context.SyntaxProvider.CreateSyntaxProvider(Predicate, Transform).Collect();
+
+        context.RegisterSourceOutput(syntaxProvider, Generate);
+    }
+}

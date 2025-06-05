418f4580eff179b1c426c6a4b1e4bddcf07fb05f
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-05 17:30:45 +0200

    feat: integrate migration tooling with database contexts and hosting

--- tools/Krosmoz.Tools.Migrations/Program.cs
+++ tools/Krosmoz.Tools.Migrations/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Krosmoz.Core.Extensions;
+using Krosmoz.Servers.AuthServer.Database;
+using Krosmoz.Servers.GameServer.Database;
+using Krosmoz.Tools.Migrations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-Console.WriteLine("Hello, World!");
+var builder = Host.CreateApplicationBuilder(args);
+
+builder
+    .AddServiceDefaults()
+    .UseNpgsqlDbContext<AuthDbContext>("krosmoz-auth-db")
+    .UseNpgsqlDbContext<GameDbContext>("krosmoz-game-db");
+
+builder.Services.AddHostedService<MigrationWorker>();
+
+var app = builder.Build();
+
+app.Run();

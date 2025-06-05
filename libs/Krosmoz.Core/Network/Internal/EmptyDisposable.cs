d60e36a2d5390f8206cdbe42efbfd80a8181c62a
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-05 11:45:40 +0200

    feat: add EmptyDisposable and EmptyServiceProvider for no-op implementations

---
+++ libs/Krosmoz.Core/Network/Internal/EmptyDisposable.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Core.Network.Internal;
+
+/// <summary>
+/// Represents an empty disposable object.
+/// Provides a no-op implementation of the <see cref="IDisposable"/> interface.
+/// </summary>
+internal sealed class EmptyDisposable : IDisposable
+{
+    private static readonly Lazy<IDisposable> s_instance = new(() => new EmptyDisposable(), LazyThreadSafetyMode.ExecutionAndPublication);
+
+    /// <summary>
+    /// Gets the singleton instance of <see cref="EmptyDisposable"/>.
+    /// </summary>
+    public static IDisposable Instance =>
+        s_instance.Value;
+
+    /// <summary>
+    /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+    /// This implementation does nothing as there are no resources to dispose.
+    /// </summary>
+    public void Dispose()
+    {
+        // No resources to dispose
+    }
+}
---
+++ libs/Krosmoz.Core/Network/Internal/EmptyServiceProvider.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Core.Network.Internal;
+
+/// <summary>
+/// Represents an empty implementation of the <see cref="IServiceProvider"/> interface.
+/// Provides a singleton instance that always returns null for service requests.
+/// </summary>
+internal sealed class EmptyServiceProvider : IServiceProvider
+{
+    private static readonly Lazy<IServiceProvider> s_instance = new(() => new EmptyServiceProvider(), LazyThreadSafetyMode.ExecutionAndPublication);
+
+    /// <summary>
+    /// Gets the singleton instance of <see cref="EmptyServiceProvider"/>.
+    /// </summary>
+    public static IServiceProvider Instance =>
+        s_instance.Value;
+
+    /// <summary>
+    /// Always returns null, indicating no service is provided for the specified type.
+    /// </summary>
+    /// <param name="serviceType">The type of service requested.</param>
+    /// <returns>Always null.</returns>
+    public object? GetService(Type serviceType)
+    {
+        return null;
+    }
+}

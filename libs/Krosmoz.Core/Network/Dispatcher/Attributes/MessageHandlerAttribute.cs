758d02b19e78a0a3b0a56ac19b97496150e8f2f0
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-05 11:59:05 +0200

    feat: add IMessageDispatcher and MessageHandlerAttribute for message handling

---
+++ libs/Krosmoz.Core/Network/Dispatcher/Attributes/MessageHandlerAttribute.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Core.Network.Dispatcher.Attributes;
+
+/// <summary>
+/// An attribute used to mark methods as message handlers.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class MessageHandlerAttribute : Attribute;
---
+++ libs/Krosmoz.Core/Network/Dispatcher/IMessageDispatcher.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Core.Network.Dispatcher;
+
+/// <summary>
+/// Defines the contract for a message dispatcher that handles the dispatching of messages
+/// to connections in the network.
+/// </summary>
+/// <typeparam name="TConnection">The type of the connection.</typeparam>
+/// <typeparam name="TMessage">The type of the message.</typeparam>
+public interface IMessageDispatcher<in TConnection, in TMessage>
+    where TConnection : class
+    where TMessage : class
+{
+    /// <summary>
+    /// Dispatches a message asynchronously to the specified connection.
+    /// </summary>
+    /// <param name="connection">The connection to which the message will be dispatched.</param>
+    /// <param name="message">The message to be dispatched.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task DispatchMessageAsync(TConnection connection, TMessage message);
+}

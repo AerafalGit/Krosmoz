a77c2130fd5fc6d189c6ae23cd8bf51637432dc6
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-05 11:52:14 +0200

    feat: add hosting extensions for CompositeServer lifecycle management

---
+++ libs/Krosmoz.Core/Network/Hosting/ServerHostedService.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Core.Network.Server;
+using Microsoft.Extensions.Hosting;
+
+namespace Krosmoz.Core.Network.Hosting;
+
+/// <summary>
+/// Represents a hosted service for managing the lifecycle of a composite server.
+/// Implements the <see cref="IHostedService"/> interface to integrate with the hosting environment.
+/// </summary>
+public sealed class ServerHostedService : IHostedService
+{
+    private readonly CompositeServer _server;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ServerHostedService"/> class.
+    /// </summary>
+    /// <param name="server">The composite server instance to manage.</param>
+    public ServerHostedService(CompositeServer server)
+    {
+        _server = server;
+    }
+
+    /// <summary>
+    /// Starts the server asynchronously.
+    /// </summary>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        return _server.StartAsync(cancellationToken);
+    }
+
+    /// <summary>
+    /// Stops the server asynchronously.
+    /// </summary>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return _server.StopAsync(cancellationToken);
+    }
+}
---
+++ libs/Krosmoz.Core/Network/Hosting/ServiceCollectionExtensions.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Core.Network.Server;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Krosmoz.Core.Network.Hosting;
+
+/// <summary>
+/// Provides extension methods for configuring services related to the composite server.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Configures the host builder to use a composite server.
+    /// </summary>
+    /// <param name="builder">The host builder to configure.</param>
+    /// <param name="configure">An action to configure the composite server builder.</param>
+    /// <returns>The configured <see cref="IHostBuilder"/>.</returns>
+    public static IHostBuilder UseCompositeServer(this IHostBuilder builder, Action<CompositeServerBuilder> configure)
+    {
+        builder.ConfigureServices(services =>
+        {
+            ConfigureServices(services, configure);
+        });
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Configures the application builder to use a composite server.
+    /// </summary>
+    /// <param name="builder">The application builder to configure.</param>
+    /// <param name="configure">An action to configure the composite server builder.</param>
+    /// <returns>The configured <see cref="IHostApplicationBuilder"/>.</returns>
+    public static IHostApplicationBuilder UseCompositeServer(this IHostApplicationBuilder builder, Action<CompositeServerBuilder> configure)
+    {
+        ConfigureServices(builder.Services, configure);
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Configures the service collection with the composite server and its options.
+    /// </summary>
+    /// <param name="services">The service collection to configure.</param>
+    /// <param name="configure">An action to configure the composite server builder.</param>
+    private static void ConfigureServices(IServiceCollection services, Action<CompositeServerBuilder> configure)
+    {
+        services.AddHostedService(provider =>
+        {
+            var builder = new CompositeServerBuilder(provider);
+            configure(builder);
+            return new ServerHostedService(builder.Build());
+        });
+    }
+}

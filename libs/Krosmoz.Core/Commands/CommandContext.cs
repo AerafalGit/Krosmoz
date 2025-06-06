feca64f7617f0135a7a51e98bcb92ddd6c5b3d58
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-06 11:40:18 +0200

    feat: add CommandContext class for command execution context

---
+++ libs/Krosmoz.Core/Commands/CommandContext.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Core.Commands.Arguments;
+
+namespace Krosmoz.Core.Commands;
+
+/// <summary>
+/// Represents the context in which a command is executed.
+/// </summary>
+public class CommandContext
+{
+    /// <summary>
+    /// Gets or sets the service provider used to resolve dependencies for the command.
+    /// </summary>
+    public required IServiceProvider Services { get; set; }
+
+    /// <summary>
+    /// Gets or sets the dictionary of argument converters used to convert string arguments
+    /// into specific types required by the command.
+    /// </summary>
+    public required IDictionary<string, IArgumentConverter> Converters { get; set; }
+
+    /// <summary>
+    /// Gets or sets the string argument associated with the command.
+    /// </summary>
+    public required string Argument { get; set; }
+}

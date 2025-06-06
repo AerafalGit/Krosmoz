1d58951c96f7be020fc283907036ac055c6d92c4
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-06 22:54:52 +0200

    feat: update `ExperienceRecord` model to use `ulong` for XP properties

--- src/Krosmoz.Servers.GameServer/Database/Models/Experiences/ExperienceRecord.cs
+++ src/Krosmoz.Servers.GameServer/Database/Models/Experiences/ExperienceRecord.cs
@@ -8,17 +8,17 @@ public sealed class ExperienceRecord
 {
     public byte Level { get; set; }
 
-    public long CharacterXp { get; set; }
+    public ulong CharacterXp { get; set; }
 
-    public long GuildXp { get; set; }
+    public ulong GuildXp { get; set; }
 
-    public long? JobXp { get; set; }
+    public ulong? JobXp { get; set; }
 
-    public long? MountXp { get; set; }
+    public ulong? MountXp { get; set; }
 
-    public long? AlignmentHonor { get; set; }
+    public ulong? AlignmentHonor { get; set; }
 
-    public ExperienceRecord(byte level, long characterXp, long guildXp, long? jobXp = null, long? mountXp = null, long? alignmentHonor = null)
+    public ExperienceRecord(byte level, ulong characterXp, ulong guildXp, ulong? jobXp = null, ulong? mountXp = null, ulong? alignmentHonor = null)
     {
         Level = level;
         CharacterXp = characterXp;

2ddf184dd9b9974688847a1def0b7ef53c3ebf07
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-06 12:15:40 +0200

    feat: add Command and CommandGroup models for organizing commands

---
+++ src/Krosmoz.Servers.GameServer/Models/Commands/Command.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Servers.GameServer.Models.Commands;
+
+/// <summary>
+/// Represents a command with a name, description, and help text.
+/// </summary>
+public sealed class Command
+{
+    /// <summary>
+    /// Gets or sets the name of the command.
+    /// </summary>
+    public required string Name { get; init; }
+
+    /// <summary>
+    /// Gets or sets the description of the command.
+    /// </summary>
+    public required string Description { get; init; }
+
+    /// <summary>
+    /// Gets or sets the help text for the command.
+    /// </summary>
+    public required string Help { get; init; }
+}
---
+++ src/Krosmoz.Servers.GameServer/Models/Commands/CommandGroup.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Servers.GameServer.Models.Commands;
+
+/// <summary>
+/// Represents a group of commands with a name and description.
+/// </summary>
+public sealed class CommandGroup
+{
+    /// <summary>
+    /// Gets or sets the name of the command group.
+    /// </summary>
+    public required string Name { get; init; }
+
+    /// <summary>
+    /// Gets or sets the description of the command group.
+    /// </summary>
+    public required string Description { get; init; }
+
+    /// <summary>
+    /// Gets or sets the array of commands in the group.
+    /// </summary>
+    public required Command[] Commands { get; init; }
+}

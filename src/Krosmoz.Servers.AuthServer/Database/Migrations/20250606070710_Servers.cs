bdc633a80a9dff68c0b893b0e312c011e78cad04
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-06 09:07:53 +0200

    feat: add migration for servers table and related model updates

---
+++ src/Krosmoz.Servers.AuthServer/Database/Migrations/20250606070710_Servers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Krosmoz.Servers.AuthServer.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Servers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "servers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    Community = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    VisibleHierarchy = table.Column<int>(type: "integer", nullable: false),
+                    JoinableHierarchy = table.Column<int>(type: "integer", nullable: false),
+                    IpAddress = table.Column<IPAddress>(type: "inet", nullable: true),
+                    Port = table.Column<int>(type: "integer", nullable: true),
+                    OpenedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_servers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "servers");
+        }
+    }
+}

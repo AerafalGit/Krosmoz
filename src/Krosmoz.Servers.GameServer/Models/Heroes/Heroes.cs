a4c07b4853efd4f90e943ea6b2ca090686bc1547
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-06 10:15:58 +0200

    feat: add Heroes model with account association property

---
+++ src/Krosmoz.Servers.GameServer/Models/Heroes/Heroes.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Protocol.Ipc.Types.Accounts;
+
+namespace Krosmoz.Servers.GameServer.Models.Heroes;
+
+/// <summary>
+/// Represents a collection of heroes.
+/// </summary>
+public sealed class Heroes
+{
+    /// <summary>
+    /// Gets or sets the account associated with the heroes.
+    /// </summary>
+    public IpcAccount Account { get; set; } = null!;
+}

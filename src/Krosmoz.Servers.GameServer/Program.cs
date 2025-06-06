826c5c1b8a7e6c9fb099ac74c451d41c196414ed
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-06 09:51:25 +0200

    feat: bootstrap GameServer with DI, Npgsql, and Nats setup

--- src/Krosmoz.Servers.GameServer/Program.cs
+++ src/Krosmoz.Servers.GameServer/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Krosmoz.Core.Extensions;
+using Krosmoz.Core.Network.Hosting;
+using Krosmoz.Core.Scheduling;
+using Krosmoz.Servers.GameServer.Database;
+using Krosmoz.Servers.GameServer.Network.Transport;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-Console.WriteLine("Hello, World!");
+var builder = Host.CreateApplicationBuilder(args);
+
+builder
+    .AddServiceDefaults()
+    .UseNpgsqlDbContext<GameDbContext>("krosmoz-game-db")
+    .UseNats("krosmoz-nats")
+    .UseCompositeServer(static server => server.ListenFromEnvironment(static connection => connection.UseConnectionHandler<DofusConnectionHandler>()));
+
+builder.Services
+    .AddTransient<IScheduler, Scheduler>();
+
+builder
+    .Build()
+    .Run();

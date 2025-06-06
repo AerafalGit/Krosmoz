283277aef84ffb0a342500751aca8f7515bf2f89
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-06 19:10:10 +0200

    refactor: simplify BooleanArgumentConverter by removing async logic

--- src/Krosmoz.Servers.GameServer/Commands/Arguments/BooleanArgumentConverter.cs
+++ src/Krosmoz.Servers.GameServer/Commands/Arguments/BooleanArgumentConverter.cs
@@ -13,13 +13,12 @@ public sealed class BooleanArgumentConverter : IArgumentConverter<bool>
     /// Converts a string argument from the command context into a boolean value.
     /// </summary>
     /// <param name="context">The context in which the command is executed, containing the argument to convert.</param>
-    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
     /// <returns>
-    /// A <see cref="ValueTask{TResult}"/> containing true if the argument matches common truthy values
+    /// True if the argument matches common truthy values
     /// (e.g., "true", "yes", "y", "1", "on", "enable", "enabled", "t"), otherwise false.
     /// </returns>
-    public ValueTask<bool> ConvertAsync(CommandContext context, CancellationToken cancellationToken)
+    public bool Convert(CommandContext context)
     {
-        return new ValueTask<bool>(context.Argument is "true" or "yes" or "y" or "1" or "on" or "enable" or "enabled" or "t");
+        return context.Argument is "true" or "yes" or "y" or "1" or "on" or "enable" or "enabled" or "t";
     }
 }

5ca07e7170f24004f9af54ec065893bf4d8eb75f
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-06 21:29:51 +0200

    feat: add IpcAccountByTicket request and response messages

---
+++ libs/Krosmoz.Protocol/Ipc/Messages/IpcAccountByTicketResponse.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Protocol.Ipc.Types.Accounts;
+
+namespace Krosmoz.Protocol.Ipc.Messages;
+
+public sealed class IpcAccountByTicketResponse
+{
+    public IpcAccount? Account { get; init; }
+}

dd73d63c652fa5495b4252b8f273d314f8e4e50e
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-06 22:29:01 +0200

    feat: add `GameAction` and `GameActionRecord` models for action handling

---
+++ src/Krosmoz.Servers.GameServer/Database/Models/GameActions/GameActionRecord.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Protocol.Enums;
+using Krosmoz.Servers.GameServer.Models.GameActions;
+
+namespace Krosmoz.Servers.GameServer.Database.Models.GameActions;
+
+public abstract class GameActionRecord
+{
+    public required GameActionTypes Type { get; set; }
+
+    public string[]? Parameters { get; set; }
+
+    public string? Criterion { get; set; }
+
+    public GameAction? Action { get; set; }
+}
---
+++ src/Krosmoz.Servers.GameServer/Models/GameActions/GameAction.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Protocol.Enums;
+
+namespace Krosmoz.Servers.GameServer.Models.GameActions;
+
+/// <summary>
+/// Represents the base class for game actions.
+/// </summary>
+public abstract class GameAction
+{
+    /// <summary>
+    /// Gets the type of the game action.
+    /// </summary>
+    public abstract GameActionTypes Type { get; }
+}

9b9275e1d4c93ee70281d7cec17fcce7ac2c4a1f
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-06 22:10:05 +0200

    feat: add experience entity and configuration to database context

--- src/Krosmoz.Servers.GameServer/Database/GameDbContext.cs
+++ src/Krosmoz.Servers.GameServer/Database/GameDbContext.cs
@@ -2,6 +2,8 @@
 // Krosmoz licenses this file to you under the MIT license.
 // See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
 
+using Krosmoz.Servers.GameServer.Database.Configurations.Experiences;
+using Krosmoz.Servers.GameServer.Database.Models.Experiences;
 using Microsoft.EntityFrameworkCore;
 
 namespace Krosmoz.Servers.GameServer.Database;
@@ -16,6 +18,12 @@ public sealed class GameDbContext : DbContext
     /// The collation used for case-insensitive string comparisons in the database.
     /// </summary>
     private const string CaseInsensitiveCollation = "case_insensitive";
+
+    /// <summary>
+    /// Gets or sets the DbSet representing the collection of <see cref="ExperienceRecord"/> entities
+    /// in the database. This property is required and provides access to the "Experiences" table.
+    /// </summary>
+    public required DbSet<ExperienceRecord> Experiences { get; set; }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="GameDbContext"/> class with the specified options.
@@ -33,5 +41,8 @@ public sealed class GameDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.HasCollation(CaseInsensitiveCollation, locale: "en-u-ks-primary", provider: "icu", deterministic: false);
+
+        builder
+            .ApplyConfiguration(new ExperienceConfiguration());
     }
 }

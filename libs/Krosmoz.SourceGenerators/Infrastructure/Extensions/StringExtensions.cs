307224596847c7acb76e943028fc920f70d8e8e5
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-06 20:53:34 +0200

    feat: add string extension methods for capitalization

---
+++ libs/Krosmoz.SourceGenerators/Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.SourceGenerators.Infrastructure.Extensions;
+
+/// <summary>
+/// Provides extension methods for string manipulation.
+/// </summary>
+public static class StringExtensions
+{
+    /// <summary>
+    /// Capitalizes the first character of the specified string.
+    /// </summary>
+    /// <param name="text">The string to capitalize.</param>
+    /// <returns>
+    /// A new string with the first character converted to uppercase,
+    /// or the original string if it is null or empty.
+    /// </returns>
+    public static string Capitalize(this string text)
+    {
+        if (string.IsNullOrEmpty(text))
+            return text;
+
+        return char.ToUpperInvariant(text[0]) + text[1..];
+    }
+
+    /// <summary>
+    /// Converts the first character of the specified string to lowercase.
+    /// </summary>
+    /// <param name="text">The string to uncapitalize.</param>
+    /// <returns>
+    /// A new string with the first character converted to lowercase,
+    /// or the original string if it is null or empty.
+    /// </returns>
+    public static string UnCapitalize(this string text)
+    {
+        if (string.IsNullOrEmpty(text))
+            return text;
+
+        return char.ToLowerInvariant(text[0]) + text[1..];
+    }
+}

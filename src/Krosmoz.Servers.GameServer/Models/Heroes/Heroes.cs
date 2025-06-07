a64aad06b67de308959144514d420e5ac3290d00
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-07 20:51:58 +0200

    feat: enhance `Heroes` model with master and slave hero management

--- src/Krosmoz.Servers.GameServer/Models/Heroes/Heroes.cs
+++ src/Krosmoz.Servers.GameServer/Models/Heroes/Heroes.cs
@@ -3,11 +3,12 @@
 // See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
 
 using Krosmoz.Protocol.Ipc.Types.Accounts;
+using Krosmoz.Servers.GameServer.Models.Actors.Characters;
 
 namespace Krosmoz.Servers.GameServer.Models.Heroes;
 
 /// <summary>
-/// Represents a collection of heroes.
+/// Represents a collection of heroes associated with an account.
 /// </summary>
 public sealed class Heroes
 {
@@ -15,4 +16,22 @@ public sealed class Heroes
     /// Gets or sets the account associated with the heroes.
     /// </summary>
     public IpcAccount Account { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the master hero in the collection.
+    /// </summary>
+    public CharacterActor Master { get; set; } = null!;
+
+    /// <summary>
+    /// Gets the list of slave heroes in the collection.
+    /// </summary>
+    public List<CharacterActor> Slaves { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Heroes"/> class.
+    /// </summary>
+    public Heroes()
+    {
+        Slaves = [];
+    }
 }

712c8234e4d9b1b109a8236803c54f8ca5f28af0
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-07 20:52:06 +0200

    feat: add `CanBeExecuted` method to `GameAction` for character validation

--- src/Krosmoz.Servers.GameServer/Models/GameActions/GameAction.cs
+++ src/Krosmoz.Servers.GameServer/Models/GameActions/GameAction.cs
@@ -3,16 +3,29 @@
 // See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
 
 using Krosmoz.Protocol.Enums;
+using Krosmoz.Servers.GameServer.Models.Actors.Characters;
 
 namespace Krosmoz.Servers.GameServer.Models.GameActions;
 
 /// <summary>
 /// Represents the base class for game actions.
+/// A game action defines a specific operation or behavior that can be executed
+/// by a character in the game world.
 /// </summary>
 public abstract class GameAction
 {
     /// <summary>
     /// Gets the type of the game action.
+    /// The type is represented by the <see cref="GameActionTypes"/> enumeration.
     /// </summary>
     public abstract GameActionTypes Type { get; }
+
+    /// <summary>
+    /// Determines whether the game action can be executed by the specified character.
+    /// </summary>
+    /// <param name="character">The character attempting to execute the action.</param>
+    /// <returns>
+    /// <c>true</c> if the action can be executed by the character; otherwise, <c>false</c>.
+    /// </returns>
+    public abstract bool CanBeExecuted(CharacterActor character);
 }

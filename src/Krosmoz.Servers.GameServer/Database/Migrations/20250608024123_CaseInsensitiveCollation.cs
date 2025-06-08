1413711f5c5617c07a4c5e289038c00c3c5f3e69
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-08 04:41:55 +0200

    feat: add initial database migrations and case-insensitive collation

---
+++ src/Krosmoz.Servers.GameServer/Database/Migrations/20250608024123_CaseInsensitiveCollation.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Krosmoz.Servers.GameServer.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class CaseInsensitiveCollation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DO $$
+                BEGIN
+                    IF NOT EXISTS (
+                        SELECT 1 FROM pg_collation
+                        WHERE collname = 'case_insensitive'
+                    ) THEN
+                        CREATE COLLATION case_insensitive (
+                            LOCALE = 'en-u-ks-primary',
+                            PROVIDER = icu,
+                            DETERMINISTIC = False
+                        );
+                    END IF;
+                END$$;
+            ");
+        }
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP COLLATION IF EXISTS case_insensitive;");
+        }
+    }
+}

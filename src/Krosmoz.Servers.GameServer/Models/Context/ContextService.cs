b3819dc73f93ec9acb9559e317b82276867517a4
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-08 04:02:42 +0200

    feat: add ContextService and IContextService for managing game contexts

---
+++ src/Krosmoz.Servers.GameServer/Models/Context/ContextService.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Protocol.Enums;
+using Krosmoz.Protocol.Messages.Game.Context;
+using Krosmoz.Servers.GameServer.Models.Actors.Characters;
+using Krosmoz.Servers.GameServer.Services.Characters.Loading;
+
+namespace Krosmoz.Servers.GameServer.Models.Context;
+
+/// <summary>
+/// Provides services for managing game contexts for character.
+/// </summary>
+public sealed class ContextService : IContextService
+{
+    private readonly ICharacterLoadingService _characterLoadingService;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ContextService"/> class.
+    /// </summary>
+    /// <param name="characterLoadingService">The service used to load character data.</param>
+    public ContextService(ICharacterLoadingService characterLoadingService)
+    {
+        _characterLoadingService = characterLoadingService;
+    }
+
+    /// <summary>
+    /// Asynchronously creates a game context for the specified character with the given player state.
+    /// </summary>
+    /// <param name="character">The character for whom the game context is to be created.</param>
+    /// <param name="state">The state of the player in the game context.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public async Task CreateContextAsync(CharacterActor character, PlayerStates state)
+    {
+        character.State = state;
+
+        await SendGameContextDestroyAsync(character);
+        await SendGameContextCreateAsync(character, state);
+
+        // TODO: implements end fight context
+
+        if (!character.IsLoaded)
+        {
+            character.IsLoaded = true;
+            await _characterLoadingService.LoadCharacterAsync(character);
+        }
+    }
+
+    /// <summary>
+    /// Asynchronously sends a message to create a game context for the specified character.
+    /// </summary>
+    /// <param name="character">The character for whom the game context is to be created.</param>
+    /// <param name="state">The state of the player in the game context.</param>
+    /// <returns>A value task representing the asynchronous operation.</returns>
+    private static ValueTask SendGameContextCreateAsync(CharacterActor character, PlayerStates state)
+    {
+        return character.Connection.SendAsync(new GameContextCreateMessage
+        {
+            Context = (sbyte)state
+        });
+    }
+
+    /// <summary>
+    /// Asynchronously sends a message to destroy the current game context for the specified character.
+    /// </summary>
+    /// <param name="character">The character for whom the game context is to be destroyed.</param>
+    /// <returns>A value task representing the asynchronous operation.</returns>
+    private static ValueTask SendGameContextDestroyAsync(CharacterActor character)
+    {
+        return character.Connection.SendAsync<GameContextDestroyMessage>();
+    }
+}
---
+++ src/Krosmoz.Servers.GameServer/Models/Context/IContextService.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Protocol.Enums;
+using Krosmoz.Servers.GameServer.Models.Actors.Characters;
+
+namespace Krosmoz.Servers.GameServer.Models.Context;
+
+/// <summary>
+/// Defines the contract for a context service that manages game contexts for character.
+/// </summary>
+public interface IContextService
+{
+    /// <summary>
+    /// Asynchronously creates a game context for the specified character with the given player state.
+    /// </summary>
+    /// <param name="character">The character for whom the game context is to be created.</param>
+    /// <param name="state">The state of the player in the game context.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task CreateContextAsync(CharacterActor character, PlayerStates state);
+}

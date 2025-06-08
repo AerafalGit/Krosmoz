aa82b1cabed8b3d893e07dd428376e89f1524814
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-08 03:08:28 +0200

    feat: add CharacterCreationOptions with configuration for initial level

---
+++ src/Krosmoz.Servers.GameServer/Models/Options/Characters/CharacterCreationOptions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Servers.GameServer.Models.Options.Characters;
+
+/// <summary>
+/// Represents the options for character creation in the game server.
+/// </summary>
+public sealed class CharacterCreationOptions
+{
+    /// <summary>
+    /// Gets or sets the initial level of the character being created.
+    /// </summary>
+    public required byte Level { get; set; }
+}

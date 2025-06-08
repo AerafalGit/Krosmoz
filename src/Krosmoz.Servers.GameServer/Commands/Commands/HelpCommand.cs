a1bc216edfc86090bd1142c1a5f0f8577076e287
jimmy-mll <jimmy.mll@hotmail.com>
2025-06-08 17:00:07 +0200

    feat: implement HelpCommand to list all available commands

--- src/Krosmoz.Servers.GameServer/Commands/Commands/HelpCommand.cs
+++ src/Krosmoz.Servers.GameServer/Commands/Commands/HelpCommand.cs
@@ -2,8 +2,12 @@
 // Krosmoz licenses this file to you under the MIT license.
 // See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
 
+using System.Text;
+using Krosmoz.Protocol.Enums;
+using Krosmoz.Protocol.Messages.Authorized;
 using Krosmoz.Servers.GameServer.Commands.Attributes;
 using Krosmoz.Servers.GameServer.Network.Transport;
+using Krosmoz.Servers.GameServer.Services.Commands;
 
 namespace Krosmoz.Servers.GameServer.Commands.Commands;
 
@@ -13,6 +17,17 @@ namespace Krosmoz.Servers.GameServer.Commands.Commands;
 [CommandGroup("commands")]
 public sealed class HelpCommand
 {
+    private readonly ICommandService _commandService;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HelpCommand"/> class.
+    /// </summary>
+    /// <param name="commandService">The service responsible for managing commands.</param>
+    public HelpCommand(ICommandService commandService)
+    {
+        _commandService = commandService;
+    }
+
     /// <summary>
     /// Executes the help command asynchronously.
     /// </summary>
@@ -20,8 +35,20 @@ public sealed class HelpCommand
     /// <returns>A completed task representing the asynchronous operation.</returns>
     [Command("help")]
     [CommandDescription("Shows the list of available commands.")]
-    public Task HelpAsync(DofusConnection connection)
+    public async Task HelpAsync(DofusConnection connection)
     {
-        return Task.CompletedTask;
+        var builder = new StringBuilder();
+
+        foreach (var commandGroup in _commandService.CommandGroups)
+        {
+            builder.AppendLine($"---- {commandGroup.Name} ----");
+
+            foreach (var command in commandGroup.Commands)
+                builder.AppendLine($"- {command.Name}: {command.Description}");
+
+            builder.AppendLine($"----{new string('-', commandGroup.Name.Length + 2)}----");
+        }
+
+        await connection.SendAsync(new ConsoleMessage { Type = (sbyte)ConsoleMessageTypes.ConsoleTextMessage, Content = builder.ToString() });
     }
 }
